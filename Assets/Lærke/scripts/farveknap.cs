9936b294d1bf8ef10c840dbbe2b9473c13b52c2e
Laerkebn <lbnonved@gmail.com>
2026-04-30 14:40:03 +0200

    intro git

---
+++ Assets/Lærke/scripts/farveknap.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SkiftGruppe : MonoBehaviour
+{
+    [Header("Grupper der forsvinder")]
+    public GameObject gruppe1;
+    public GameObject gruppe2;
+    public GameObject gruppe3;
+    public GameObject gruppe4;
+    public GameObject gruppe5;
+
+    [Header("Gruppe der kommer frem")]
+    public GameObject gruppeFarve;
+
+    void Start()
+    {
+        gruppe1.SetActive(true);
+        gruppe2.SetActive(true);
+        gruppe3.SetActive(true);
+        gruppe4.SetActive(true);
+        gruppe5.SetActive(true);
+        gruppeFarve.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        gruppe1.SetActive(false);
+        gruppe2.SetActive(false);
+        gruppe3.SetActive(false);
+        gruppe4.SetActive(false);
+        gruppe5.SetActive(false);
+        gruppeFarve.SetActive(true);
+    }
+}
